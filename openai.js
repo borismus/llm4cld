d52e922ca6b6c4c6683beaddc7d3acf9df7e35ba
Boris Smus <boris@smus.com>
2023-03-23 22:16:59 -0700

    add secret

--- openai.js
+++ openai.js
@@ -1,5 +1,6 @@
-const ENDPOINT_URL = 'https://us-central1-musemuse.cloudfunctions.net/openai_complete';
-// const ENDPOINT_URL = 'http://localhost:8080';
+// const ENDPOINT_URL = 'https://us-central1-musemuse.cloudfunctions.net/openai_complete';
+const ENDPOINT_URL = 'http://localhost:8080';
+import {OPENAI_KEY} from './secret.js';
 
 export async function extractEntities(groundingText, entityCount = 5) {
 }
@@ -7,9 +8,11 @@ export async function extractEntities(groundingText, entityCount = 5) {
 export async function callGPT4(prompt) {
   const formData = new FormData();
   const res = await fetch(ENDPOINT_URL + '?' + new URLSearchParams({
-    'openai_key': 'value',
-    bar: 2,
-}));
+    openai_key: OPENAI_KEY,
+    model: 'gpt-4',
+    temperature: 0.3,
+    prompt,
+  }));
 
   console.log(res);
 }
