96167caf7cf44938a783ff3b917ea1f3402f1d8b
Boris Smus <boris@smus.com>
2023-03-24 17:25:27 -0700

    add parsing for booleans and lists, as well as negative causal links

--- openai.js
+++ openai.js
@@ -1,18 +1,103 @@
-// const ENDPOINT_URL = 'https://us-central1-musemuse.cloudfunctions.net/openai_complete';
-const ENDPOINT_URL = 'http://localhost:8080';
+const ENDPOINT_URL = 'https://us-central1-musemuse.cloudfunctions.net/openai_complete';
+import {DEFAULT_TEMPERATURE} from './constants.js';
+// const ENDPOINT_URL = 'http://localhost:8080';
 import {OPENAI_KEY} from './secret.js';
 
 export async function extractEntities(groundingText, entityCount = 5) {
+  const prompt = generateExtractEntitiesPrompt(groundingText, entityCount);
+  const gptRes = await callGPT4(prompt);
+  // Parse response into a list.
+  let entities = gptRes.split('\n');
+  entities = entities.map(ent => cleanupEntity(ent));
+  return entities;
 }
 
-export async function callGPT4(prompt) {
-  const formData = new FormData();
-  const res = await fetch(ENDPOINT_URL + '?' + new URLSearchParams({
+export async function isCausalLink(groundingText, entity1, entity2, {isOpposite = false} = {}) {
+  const prompt = isOpposite ?
+    generateCausalNegativeLinkPrompt(groundingText, entity1, entity2) :
+    generateCausalLinkPrompt(groundingText, entity1, entity2);
+
+  const gptRes = await callGPT4(prompt);
+
+  // Parse response into a boolean.
+  let res = null;
+  switch (gptRes) {
+    case 'true':
+      res = true;
+      break;
+    case 'false':
+      res = false;
+      break;
+    default:
+      throw new Error(`Could not parse response into boolean: "${gptRes}"`);
+  }
+  if (res) {
+    console.log(`isCausalLink${isOpposite ? ' (opposite)' : ''}: ${entity1} --> ${entity2}`);
+  }
+  return res;
+}
+
+export async function isCausalLinkWithExplanation(groundingText, entity1, entity2) {
+
+}
+
+export async function callGPT4(prompt, {verbose = false} = {}) {
+  if (!prompt) {
+    throw new Error(`Prompt required.`);
+  }
+  const request = {
     openai_key: OPENAI_KEY,
     model: 'gpt-4',
-    temperature: 0.3,
+    temperature: DEFAULT_TEMPERATURE,
     prompt,
-  }));
+  };
+  if (verbose) {
+    console.log(`[GPT] Request prompt "${prompt}"`);
+  }
+  const start = performance.now();
+  const res = await fetch(ENDPOINT_URL + '?' + new URLSearchParams(request));
+  const duration = performance.now() - start;
 
-  console.log(res);
+  const text = await res.text();
+  if (verbose) {
+    console.log(`[GPT] Response text "${text}"`);
+  } else {
+    console.log(`[GPT] Response: ${text.length} chars. Took ${Math.floor(duration)} ms.`);
+  }
+
+  return text;
+}
+
+function generateExtractEntitiesPrompt(groundingText, entityCount) {
+  return `Text: ${groundingText}
+
+The following ${entityCount} entities appear in the text above:
+-`;
+}
+
+function generateCausalLinkPrompt(groundingText, entity1, entity2) {
+  return `Text: ${groundingText}
+
+The text above suggests that more ${entity1} causes more ${entity2}. Answer one of "true" or "false".`;
+}
+
+function generateCausalNegativeLinkPrompt(groundingText, entity1, entity2) {
+  return `Text: ${groundingText}
+
+The text above suggests that more ${entity1} causes less ${entity2}. Answer one of "true" or "false".`;
+}
+
+function cleanupEntity(ent) {
+  // Usually in the form of a bulleted or numbered list, eg ("30. Foo" or "- Bar").
+
+  // First check for bullets.
+  if (ent.startsWith('- ')) {
+    return ent.substring(2);
+  }
+  // Otherwise its numbered.
+  const match = ent.match(/^[0-9]+\. (.*)$/);
+  if (match !== null) {
+    return match[1];
+  }
+  throw new Error(`Entity not in recognized format: "${ent}"`)
 }
